5a8fbd8a11b46448405f01b51d5c52aa85fd9a0f
Clinton Findlay <clintonf84@gmail.com>
2021-03-29 10:39:46 -0600

    pushing code

---
+++ ApiTest/TestApiCalls.cs
@@ -0,0 +1,20 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ApiTest
+{
+    [TestClass]
+    public class TestApiCalls
+    {
+        [TestMethod]
+        public void GetSkillsTest()
+        {
+            //Setup
+            bool test = true;
+
+            //Act
+
+            //Assert
+            Assert.IsTrue(test);
+        }
+    }
+}
